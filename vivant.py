990027c4fc469e92b25fc1f801189747c13acd7a
Nicolas Bernard <nicolas.bernard38460@gmail.com>
2017-04-11 18:11:28 +0200

    ajout du module vivant.py

---
+++ vivant.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+module regroupant les fonctions permettant de manipuler
+les segments vivants
+"""
+import sys
+from geo.segment import *
+from geo.tycat import *
+from sortedcontainers.sortedlist import *
+
+def ajouter_aux_vivants:
+    """
+    """
+    #TODO
+
+def supprimer_des_vivant:
+    """
+    """
+    #TODO
+
+
+def initialiser_vivants:
+    """
+    """
+    #TODO
