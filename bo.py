0b7922860d9a84e11d3e8f5d620291658fe3053a
Nicolas Bernard <nicolas.bernard38460@gmail.com>
2017-04-11 10:38:30 +0200

    creation de la fonction create_event, permettant de creer la liste des evenements triés par Y croisssants

--- bo.py
+++ bo.py
@@ -8,8 +8,8 @@ for each file:
     - print some statistics
 """
 import sys
-from geo.segment import load_segments
-from geo.tycat import tycat
+from geo.segment import *
+from geo.tycat import *
 
 def test(filename):
     """
@@ -24,6 +24,36 @@ def test(filename):
     #print("le nombre d'intersections (= le nombre de points differents) est", ...)
     #print("le nombre de coupes dans les segments (si un point d'intersection apparait dans
     # plusieurs segments, il compte plusieurs fois) est", ...)
+
+
+def getY(item):
+    """
+    fonction utilisé pour récupérer le Y du point pour la fonction sorted()
+    """
+    return(item.coordinates[1])
+
+def create_event(liste_des_segments):
+    """
+    ENTRÉE : Une liste de segment de la forme [Segment(Point1, Point2), ...., Segment(Point1, Point2)]
+    SORTIE : Une liste des points triés par "ordre croissant de balayage",
+    représentant l'ensemble E des évenements à parcourir.
+    """
+    liste_event_non_triee = []
+    for s in liste_des_segments:
+        for p in s.endpoints:
+            liste_event_non_triee.append(p)
+    ## Pour l'instant la liste des évenements n'est pas triée.
+    liste_event_triee = sorted(liste_event_non_triee, key=getY)
+    # on vient de trier la liste en fonction des Y croissants
+    # mais il reste encore a trier la liste pour un meme Y
+    # avoir comment on le fait.
+    return(liste_event_triee)
+
+
+
+
+
+
 
 def main():
     """
